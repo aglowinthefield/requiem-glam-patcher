f693f8b4964a4b549b1d3f050f71ac0de0fdcc29
aglowinthefield <tom.auty+nexus@gmail.com>
2025-07-08 17:16:00 -0400

    simple build

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Plugins;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine($"Mutagen.Bethesda version: {typeof(ISkyrimModGetter).Assembly.GetName().Version}");
+
+// Example of how to create a new mod
+var mod = new SkyrimMod(new ModKey("RequiemGlamPatcher", ModType.Plugin), SkyrimRelease.SkyrimSE);
+Console.WriteLine($"Created mod: {mod.ModKey}");
+
+// Print current working directory
+Console.WriteLine($"Current working directory: {Directory.GetCurrentDirectory()}");
+
+// Wait for user input before closing
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
